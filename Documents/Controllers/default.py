736bba16daa4f226bd20a3b0834be0b6cbc4e0f0
Andrei Cunha <cunhalimaandrei1@gmail.com>
2020-09-02 14:57:52 -0300

    Atualização no default

--- Documents/Controllers/default.py
+++ Documents/Controllers/default.py
@@ -6,20 +6,22 @@ def main():
 	"""
 	Nome: main
 
+	Entradas: Sem parâmetros de entrada.
+
 	Função: É a principal função do programa principal. Realiza o manejo de informações
-	entre o inversor de frequência, e salva os dados no InfluxDB
+	entre o inversor de frequência, e salva os dados no InfluxDB.
 
 	Objetos:
-	Client_ModBus (Objeto responsável pela comunicação ModBus TCP/IP)
+	Client_ModBus (Objeto responsável pela comunicação ModBus TCP/IP).
 
 	Variáveis:
 
 
 	"""
-	Client_ModBus = ModbusClient(host = 'localhost', port = 502, auto_open = True, auto_close = True)
+	Client_ModBus = ModbusClient(host = HOST, port = 502, auto_open = True, auto_close = True)
 	Client_ModBus.open()
 	Client_ModBus.debug(True)
-	Registradores = Client_ModBus.read_holding_registers(10,2)
+	Registradores = Client_ModBus.read_holding_registers(1001,2)
 	if Registradores:
 		print(Registradores)
 	else:
